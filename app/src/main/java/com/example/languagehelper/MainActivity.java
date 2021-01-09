faa2dd208c2e7a2c1c70c5b1ffd6b21040347c14
David le Roux <daveleroux@gmail.com>
2021-01-09 18:41:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/languagehelper/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.languagehelper;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_import:
+                importData();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void importData() {
+        new AlertDialog.Builder(this)
+                .setTitle("Result")
+                .setMessage("Imported")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        finish();
+                    }
+                })
+                .show();
+    }
+
+    public void addTermMessage(View view) {
+        // Do something in response to button
+    }
+}
